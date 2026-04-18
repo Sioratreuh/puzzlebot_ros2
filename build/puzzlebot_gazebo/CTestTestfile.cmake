460c5d1fc2fc29e08592e1cc7e66364797efa04a
Dreary22 <drearyland22021@gmail.com>
2026-04-18 21:07:00 +0000

    hotfix orden

--- build/puzzlebot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Semestre8/Modulo3/puzzlebot_ros2/src/puzzlebot_gazebo
-# Build directory: /root/Semestre8/Modulo3/puzzlebot_ros2/build/puzzlebot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
